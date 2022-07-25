38706bd9667a21c67a3d139e3568636cdf9b87ef
German Gonzalez <ggonzalez@transnegrd.com>
2022-07-25 12:04:12 -0400

    lasdas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
+// modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router'
+import { ReactiveFormsModule } from '@angular/forms';
+//componentes
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { LoginComponent } from './components/inicio/login/login.component';
+import { RegisterComponent } from './components/inicio/register/register.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'bienvenidos', pathMatch: 'full'},
+      {path: 'bienvenidos', component: BienvenidaComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: '**', redirectTo: 'bienvenidos', pathMatch: 'full'},
+    ]),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/inicio/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  login: FormGroup; 
+
+  constructor( private fb: FormBuilder ) {
+    this.login = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    }); 
+   } 
+
+  ngOnInit(): void {
+  }
+  log(): void{
+    
+    const usuario: Usuario = {
+       nombreUsuario: this.login.value.usuario,
+       password: this.login.value.password,
+    }
+    console.log(usuario);
+  }
+
+}
